ab378254bc3f36b549ba54c00afe9f0b78b0df7e
noufa03 <salim2002621@gmail.com>
2025-06-22 15:55:50 +0530

    landing page preview

---
+++ resources/views/components/aboutus.blade.php
@@ -0,0 +1,53 @@
+<div class="mt-50 min-h-screen flex flex-col gap-y-25 justify-center ">
+
+    <h1 class="lg:text-5xl font-bold text-primary leading-tight text-center">
+        About US
+    </h1>
+
+    <div class="flex justify-between">
+
+        <div class="card bg-base-300  w-[350px] h-[330px] shadow-xl">
+            <div class="card-body items-center text-center">
+                <h2 class="card-title text-primary text-3xl pt-15">Our Mission</h2>
+                <p class="text-neutral text-[16px]">
+                    Connect passionate individuals with opportunities to make a positive impact, fostering a world where
+                    every smile counts
+                </p>
+            </div>
+        </div>
+
+        <div class="card bg-base-300  w-[350px] h-[330px] shadow-xl">
+            <div class="card-body items-center text-center">
+                <h2 class="card-title text-primary text-[30px] pt-15">Our Values</h2>
+                <p class="text-neutral text-base">
+                    Guided by kindness, inclusivity, and dedication, we support volunteers in transforming lives
+                </p>
+            </div>
+        </div>
+
+        <div class="card bg-base-300  w-[350px] h-[330px] shadow-xl">
+            <div class="card-body items-center text-center">
+                <h2 class="card-title text-primary text-3xl pt-15">Our Impact</h2>
+                <p class="text-neutral text-[16px]">
+                    Thousands of hours donated, countless smiles shared, and communities strengthened through our
+                    volunteer efforts
+                </p>
+            </div>
+        </div>
+
+        <div class="card bg-base-300  w-[350px] h-[330px] shadow-xl">
+            <div class="card-body items-center text-center">
+                <h2 class="card-title text-primary text-[30px] pt-15">Our Vision</h2>
+                <p class="text-neutral text-base">
+                    A global network of volunteers creating lasting change, one kind act at a time
+                </p>
+            </div>
+        </div>
+
+    </div>
+    <button class="btn btn-accent w-[184px] h-[61px] rounded-[99px] text-success-content  lg:text-base block mx-auto">
+        Learn more
+    </button>
+
+
+</div>
--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -1,56 +1,67 @@
+<div class="mt-25">
+    <div class="flex justify-between">
+        <div class=" w-1/3 h-[510px] flex flex-col lg:gap-[15px]">
+            <h1 class=" lg:text-6xl font-bold text-primary leading-tight ">
+                Welcome to Smile
+                <span class="text-accent">Volunteer</span>
+                <br>
+                with Heart!
+            </h1>
+            <p class="lg:text-xl py-6 ">
+                Join a community of compassionate individuals making a <br>
+                difference. Whether you're lending a hand, sharing a <span class="text-accent">smile</span>,<br>
+                or building lasting friendships, your effort changes lives.<br>
+                Start your volunteering journey today!
+            </p>
+            <div class="flex w-1/2 gap-5">
+                <button
+                    class="btn btn-accent w-[230px] h-[46px] rounded-[99px] text-accent-content px-[12px] py-[24px] lg:text-base">
+                    I'd like to volunteer
+                </button>
+                <button
+                    class="btn btn-outline border-2 btn-accent w-[230px] h-[46px] rounded-[99px] px-[12px] py-[24px] lg:text-base ">
+                    I'm looking for volunteers
+                </button>
 
-<main>
-    <section class="px-10">
-        <div class="container py-20 mx-auto">
-            <div class="flex flex-col-reverse md:flex-row items-center z-20 gap-10">
-                <div class="text-center md:text-left md:w-1/2">
-                    <div class="avatar-group -space-x-6 mb-10">
-                        <div class="avatar">
-                            <div class="w-12">
-                                <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
-                            </div>
-                        </div>
-                        <div class="avatar">
-                            <div class="w-12">
-                                <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
-                            </div>
-                        </div>
-                        <div class="avatar">
-                            <div class="w-12">
-                                <img src="https://img.daisyui.com/images/profile/demo/averagebulk@192.webp" />
-                            </div>
-                        </div>
-                        <div class="avatar avatar-placeholder">
-                            <div class="bg-neutral text-neutral-content w-12">
-                                <span>+99</span>
-                            </div>
-                        </div>
-                    </div>
-                    <h1 class="text-3xl md:text-4xl font-bold leading-snug mb-4">
-                        Bring a Smile, Be the Change
-                    </h1>
-                    <p class="leading-relaxed mb-10">
-                        Join Smiles – a community of passionate volunteers spreading hope, kindness, and happiness to those who need it most.
-                    </p>
-                    <div class="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
-                        <a href="/signup/volunteer" class="btn btn-primary btn-lg">
-                            I'd like to volunteer
-                        </a>
-                        <a href="/signup/organization">
-                            <button class="btn btn-soft btn-primary btn-lg">
-                                I'm looking for volunteers
-                            </button>
-                        </a>
-                    </div>
+            </div>
+
+        </div>
+        <div class="flex gap-[29px] w-1/2">
+
+            <div class="flex items-center">
+                <div class="relative w-[410px] h-[420px]">
+                    <img
+                        src="{{asset('storage/assets/1.jpg')}}"
+                        class="w-full h-full rounded-lg object-cover "
+                    />
+                    <div
+                        class="absolute top-0 left-0 w-full h-full bg-secondary-content opacity-20 rounded-lg pointer-events-none"></div>
                 </div>
 
-                <div class="md:w-1/2 flex justify-center">
-                    <img src="{{ asset('storage/assets/landing_image.svg') }}"
-                         alt="A person helping another person stand up"
-                         class="max-w-full rounded-2xl" loading="lazy" />
+            </div>
+
+            <div class="flex flex-col gap-[29px]">
+                <div class="relative w-[250px] h-[270px]">
+                    <img
+                        src="{{ asset('storage/assets/2.jpg') }}"
+                        class="w-full h-full object-cover rounded-lg"
+                    />
+                    <div
+                        class="absolute top-0 left-0 w-full h-full bg-secondary-content opacity-20 rounded-lg pointer-events-none"></div>
+                </div>
+                <div class="relative w-[250px] h-[270px] ">
+
+                    <img
+                        src="{{asset('storage/assets/3.jpg')}}"
+                        class="w-full h-full rounded-lg object-cover"
+                    />
+                    <div
+                        class="absolute top-0 left-0 w-full h-full bg-secondary-content opacity-20 rounded-lg pointer-events-none"></div>
+
                 </div>
 
             </div>
         </div>
-    </section>
-</main>
+
+    </div>
+</div>
