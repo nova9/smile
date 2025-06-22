c81cce013e7993abbace3afded25d7f9a9eb3544
Thathsara <thathsaramadhhusha@gmail.com>
2025-06-22 18:00:36 +0530

    create folder for roles

--- resources/views/components/dashboard/item.blade.php
+++ resources/views/components/dashboard/item.blade.php
@@ -1,5 +1,5 @@
 <li>
-    <a href="{{ $to }}" class="hover:bg-gray-100 px-2" wire:current="menu-active">
+    <a href="{{ $to }}" class="hover:bg-gray-100 px-2" wire:current.exact="menu-active" wire:navigate>
         <i data-lucide="{{ $icon }}" class="size-6"></i>
         <span x-show="!navClosed">{{ $title }}</span>
     </a>
