6aadda2181703529957bc6021fd8fd82ef7f2afe
noufa03 <snoufa3@gmail.com>
2025-07-02 10:59:54 +0530

    sidebar done

--- routes/volunteer.php
+++ routes/volunteer.php
@@ -6,4 +6,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', \App\Livewire\Volunteer\Dashboard\Index::class);
     Route::get('/dashboard/events', \App\Livewire\Volunteer\Dashboard\Events::class);
     Route::get('/dashboard/profile', \App\Livewire\Volunteer\Dashboard\Profile::class);
+    Route::get('/dashboard/opportunities', \App\Livewire\Volunteer\Dashboard\Opportunities::class);
+    Route::get('/dashboard/feedback', \App\Livewire\Volunteer\Dashboard\Feedback::class);
+    Route::get('/dashboard/community', \App\Livewire\Volunteer\Dashboard\Community::class);
+    Route::get('/dashboard/activities', \App\Livewire\Volunteer\Dashboard\Activities::class);
 });
