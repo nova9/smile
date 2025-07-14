abc8a5e384604f624b427123078d82cd5ea619b2
Thathsara <thathsaramadhhusha@gmail.com>
2025-07-14 10:52:21 +0530

    alpine persist added for dashboard toggle

--- resources/views/components/common/dashboard/index.blade.php
+++ resources/views/components/common/dashboard/index.blade.php
@@ -36,7 +36,7 @@
 
 
                 <div x-show="open"
-                     class="absolute bottom-0 -right-1 translate-x-full bg-white rounded-box border border-gray-200 shadow-lg">
+                     class="absolute bottom-0 -right-1 translate-x-full bg-white rounded-box border border-gray-200 shadow-lg w-fit">
                     <div class="flex items-center py-1 gap-2 m-2">
                         <div class="size-8">
                             <img src="https://picsum.photos/300/300" class="rounded-lg" alt="">
@@ -54,7 +54,7 @@
                         <li>
                             <a href="/requester/upgrade" class="hover:bg-gray-100">
                                 <i data-lucide="sparkles" class="size-5"></i>
-                                <span>Upgrade to Pro</span>
+                                <span class="text-nowrap">Upgrade to Pro</span>
                             </a>
                         </li>
                         <li>
