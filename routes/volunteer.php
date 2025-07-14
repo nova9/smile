a73a12cb273b15d887dfdee49f51fdb9a8f12a00
salim359 <snoufa3@gmail.com>
2025-07-14 10:44:30 +0530

    new changes

--- routes/volunteer.php
+++ routes/volunteer.php
@@ -6,6 +6,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', \App\Livewire\Volunteer\Dashboard\Index::class);
     Route::get('/dashboard/events', \App\Livewire\Volunteer\Dashboard\Events::class);
     Route::get('/dashboard/profile', \App\Livewire\Volunteer\Dashboard\Profile::class);
+    Route::get('/dashboard/leaderboard', \App\Livewire\Volunteer\Dashboard\Leaderboard::class);
     Route::get('/dashboard/opportunities', \App\Livewire\Volunteer\Dashboard\Opportunities::class);
     Route::get('/dashboard/feedback', \App\Livewire\Volunteer\Dashboard\Feedback::class);
     Route::get('/dashboard/community', \App\Livewire\Volunteer\Dashboard\Community::class);
