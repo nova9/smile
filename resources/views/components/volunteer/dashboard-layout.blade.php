6e8f46b5d9e2dee9c510a0fc026bdf4a5eb0cd49
Thathsara <thathsaramadhhusha@gmail.com>
2025-07-18 14:23:32 +0530

    join event

--- resources/views/components/volunteer/dashboard-layout.blade.php
+++ resources/views/components/volunteer/dashboard-layout.blade.php
@@ -2,7 +2,7 @@
     <x-common.dashboard>
         <x-slot name="items">
             <x-common.dashboard.item to="/volunteer/dashboard" icon="house" title="Dashboard"/>
-            <x-common.dashboard.item to="/volunteer/dashboard/opportunities" icon="party-popper" title="Opportunities"/>
+            <x-common.dashboard.item to="/volunteer/dashboard/events" icon="party-popper" title="Opportunities"/>
             <x-common.dashboard.item to="/volunteer/dashboard/leaderboard" icon="users" title="Leaderboard"/>
             <x-common.dashboard.item to="/volunteer/dashboard/activities" icon="tractor" title="My Activities"/>
             <x-common.dashboard.item to="/volunteer/dashboard/feedback" icon="message-square-quote" title="Feedback"/>
--- routes/volunteer.php
+++ routes/volunteer.php
@@ -3,12 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', \App\Livewire\Volunteer\Dashboard\Index::class);
-    Route::get('/dashboard/events', \App\Livewire\Volunteer\Dashboard\Events::class);
-    Route::get('/dashboard/profile', \App\Livewire\Volunteer\Dashboard\Profile::class);
-    Route::get('/dashboard/leaderboard', \App\Livewire\Volunteer\Dashboard\Leaderboard::class);
-    Route::get('/dashboard/opportunities', \App\Livewire\Volunteer\Dashboard\Opportunities::class);
-    Route::get('/dashboard/feedback', \App\Livewire\Volunteer\Dashboard\Feedback::class);
-    Route::get('/dashboard/community', \App\Livewire\Volunteer\Dashboard\Community::class);
-    Route::get('/dashboard/activities', \App\Livewire\Volunteer\Dashboard\Activities::class);
+    Route::prefix('/dashboard')->group(function () {
+        Route::get('/', \App\Livewire\Volunteer\Dashboard\Index::class);
+        Route::get('/events', \App\Livewire\Volunteer\Dashboard\Events::class);
+        Route::get('/profile', \App\Livewire\Volunteer\Dashboard\Profile::class);
+        Route::get('/leaderboard', \App\Livewire\Volunteer\Dashboard\Leaderboard::class);
+
+        Route::get('/events', \App\Livewire\Volunteer\Dashboard\Eventz\Index::class);
+        Route::get('/events/{id}', \App\Livewire\Volunteer\Dashboard\Eventz\Show::class);
+
+        Route::get('/feedback', \App\Livewire\Volunteer\Dashboard\Feedback::class);
+        Route::get('/community', \App\Livewire\Volunteer\Dashboard\Community::class);
+        Route::get('/activities', \App\Livewire\Volunteer\Dashboard\Activities::class);
+    });
 });
