83e1944efe51713fed51d11bd8a4cf4c11313eb3
Thathsara <thathsaramadhhusha@gmail.com>
2025-07-18 14:40:27 +0530

    application

--- routes/volunteer.php
+++ routes/volunteer.php
@@ -12,6 +12,8 @@ Route::middleware('auth')->group(function () {
         Route::get('/events', \App\Livewire\Volunteer\Dashboard\Eventz\Index::class);
         Route::get('/events/{id}', \App\Livewire\Volunteer\Dashboard\Eventz\Show::class);
 
+        Route::get('/applications', \App\Livewire\Volunteer\Dashboard\Applications\Index::class);
+
         Route::get('/feedback', \App\Livewire\Volunteer\Dashboard\Feedback::class);
         Route::get('/community', \App\Livewire\Volunteer\Dashboard\Community::class);
         Route::get('/activities', \App\Livewire\Volunteer\Dashboard\Activities::class);
