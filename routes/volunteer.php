a5ff1ee8bc2b093315ce72cea273913ecc13d35e
Thathsara <thathsaramadhhusha@gmail.com>
2025-07-18 16:19:14 +0530

    remove reduntandt event file

--- routes/volunteer.php
+++ routes/volunteer.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::prefix('/dashboard')->group(function () {
         Route::get('/', \App\Livewire\Volunteer\Dashboard\Index::class);
-        Route::get('/events', \App\Livewire\Volunteer\Dashboard\Events::class);
+        Route::get('/events', \App\Livewire\Volunteer\Dashboard\Eventz\Index::class);
         Route::get('/profile', \App\Livewire\Volunteer\Dashboard\Profile::class);
         Route::get('/leaderboard', \App\Livewire\Volunteer\Dashboard\Leaderboard::class);
 
