41c3df2eba63567e3f2547f990bc48c21081fe66
gamiduwelikanna <gamidukaveen@gmail.com>
2025-07-18 23:46:09 +0530

    service requester making in progress

--- routes/requester.php
+++ routes/requester.php
@@ -3,14 +3,28 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Requester\Dashboard\Index;
 use App\Livewire\Requester\Dashboard\Events;
-use App\Livewire\Requester\Dashboard\NewRequest;
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', Index::class);
-    Route::get('/dashboard/events', Events::class);
 });
 
 Route::middleware(['auth', 'role:requester'])->group(function () {
     Route::get('/events', Events::class)->name('requester.events');
-    Route::get('/dashboard/new-request', NewRequest::class)->name('requester.create-event');
 });
+
+Route::get('/create-event', function () {
+    return 'Create Event Page Placeholder';
+})->name('requester.create-event');
+
+Route::get('/applicants', function () {
+    return 'Applicants placeholder page';
+})->name('requester.applicants');
+
+Route::get('/feedback', function () {
+    return 'Feedback placeholder';
+})->name('requester.feedback');
+
+Route::get('/profile', function () {
+    return 'Profile page placeholder';
+})->name('requester.profile');
+
