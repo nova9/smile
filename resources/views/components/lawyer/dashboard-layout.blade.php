d47fd55c2635a3651cb966c246561e05be5c64aa
lakindukivin <140165115+lakindukivin@users.noreply.github.com>
2025-07-22 00:02:35 +0530

    Lawyer Dashboard

--- resources/views/components/lawyer/dashboard-layout.blade.php
+++ resources/views/components/lawyer/dashboard-layout.blade.php
@@ -1,8 +1,7 @@
 <div>
     <x-common.dashboard>
         <x-slot name="items">
-            <x-common.dashboard.item to="/lawyer/dashboard" icon="house" title="Dashboard"/>
-            <x-common.dashboard.item to="/lawyer/legal-reviews" icon="scroll-text" title="Legal Reviews"/>
+            <x-common.dashboard.item to="/lawyer/dashboard" icon="house" title="Dashboard" />
         </x-slot>
 
         {{ $slot }}
