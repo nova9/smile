486f40e23a548b1145c72c6ac864720f7538aae1
Thathsara <thathsaramadhhusha@gmail.com>
2025-07-22 11:10:47 +0530

    profile fix

--- app/Livewire/Requester/Dashboard/MyEvents/Show.php
+++ app/Livewire/Requester/Dashboard/MyEvents/Show.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class Show extends Component
 {
+    public $event;
+    public function mount($id)
+    {
+        $this->event = auth()->user()->organizingEvents()->find($id);
+        dd($this->event->users);
+    }
+
     public function render()
     {
         return view('livewire.requester.dashboard.my-events.show');
