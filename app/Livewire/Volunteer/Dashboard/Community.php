fd3c8e019201aeb7a0b005d31cc83a4e0ed28952
salim359 <snoufa3@gmail.com>
2025-07-24 17:10:32 +0530

    community space page done

--- app/Livewire/Volunteer/Dashboard/Community.php
+++ app/Livewire/Volunteer/Dashboard/Community.php
@@ -3,9 +3,18 @@
 namespace App\Livewire\Volunteer\Dashboard;
 
 use Livewire\Component;
+use App\Models\Event;
 
 class Community extends Component
 {
+
+
+    public $event;
+    public function mount($id){
+        $this->event=Event::query()
+        ->with(['address','users','category','tags'])
+        ->find($id);
+    }
     public function render()
     {
         return view('livewire.volunteer.dashboard.community');
