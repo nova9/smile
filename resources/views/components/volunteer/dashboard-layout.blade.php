9fb1903a231da5dfd850e6ac84c6f0a9080f6501
Gamidu Welikanna <gamidukaveen@gmail.com>
2025-10-17 11:20:17 +0530

    testing

--- resources/views/components/volunteer/dashboard-layout.blade.php
+++ resources/views/components/volunteer/dashboard-layout.blade.php
@@ -6,6 +6,7 @@
             <x-common.dashboard.item to="/volunteer/dashboard/events" icon="party-popper" title="Opportunities"/>
             <x-common.dashboard.item to="/volunteer/dashboard/leaderboard" icon="users" title="Leaderboard"/>
             <x-common.dashboard.item to="/volunteer/dashboard/achievements" icon="trophy" title="My Achievements"/>
+            <x-common.dashboard.item to="/volunteer/dashboard" icon="help" title="Help Center"/>
             
         </x-slot>
 
